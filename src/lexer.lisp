9f8306a1d9b4315a20d3cee39cb8860176dc300a
Robert Burnett <dr.slate2@gmail.com>
2024-11-26 22:37:16 -0600

    improved the lexer

--- src/lexer.lisp
+++ src/lexer.lisp
@@ -15,16 +15,18 @@
     :accessor index
     :type integer)))
 
-(defgeneric next-char (lexer)
-  (:documentation "Return the next character from the input-string"))
+(defgeneric get-char (lexer)
+  (:documentation "Return the current character from the input-string")
+  (:method ((lexer lexer))
+    (with-slots ((i index) (str input-string)) lexer
+      (if (< i (length str))
+	  (char str i)
+	  nil))))
 
-(defmethod next-char ((lexer lexer))
-  (if (>= (index lexer) (length (input-string lexer)))
-      nil
-      (let
-	  ((result (char (input-string lexer) (index lexer))))
-	(incf (index lexer))
-	result)))
+(defgeneric advance-char! (lexer)
+  (:documentation "advance to the next character from the input-string")
+  (:method ((lexer lexer))
+    (incf (index lexer))))
 
 
 ;;; Lexer for slic
@@ -34,16 +36,44 @@
 (defgeneric next-token (lexer)
   (:documentation "Return the next token from the input-string"))
 
+(defun collect-all-matches (lexer predicate)
+  "Collect all matches to the predicate from the lexer"
+  (coerce 'string
+	  (loop
+	    for char = (get-char lexer)
+	    while (and (not (null char)) (apply predicate (list char)))
+	    do (advance-char! lexer)
+	    collect char)))
+
+(defun whitespace-char-p (ch)
+  (or
+   (char= ch #\Space)
+   (char= ch #\Tab)
+   (char= ch #\Newline)))
+
 (defmethod next-token ((lexer slic-lexer))
-  (let ((ch (next-char lexer)))
+  (let ((ch (get-char lexer)))
     (when (null ch) (return-from next-token nil))
     (cond
-      ((char= ch #\() "(")
-      ((char= ch #\)) ")")
+      ((char= ch #\()
+       (advance-char! lexer)
+       "(")
+      ((char= ch #\))
+       (advance-char! lexer)
+       ")")
+      ((char= ch #\-)
+       (advance-char! lexer)
+       "-")
+      ((char= ch #\:)
+       (advance-char! lexer)
+       ":")
+      ((char= ch #\.)
+       (advance-char! lexer)
+       ".")
+      ((whitespace-char-p ch)
+       (collect-all-matches lexer #'whitespace-char-p) " ")
+      ((digit-char-p ch)
+       (collect-all-matches lexer #'digit-char-p))
       ((alpha-char-p ch)
-       (concatenate 'string (string ch)
-	(loop
-	  for char = (next-char lexer)
-	  while (and (not (null ch)) (alpha-char-p char))
-	  collect char)))
+       (collect-all-matches lexer #'alpha-char-p))
       (t (error "no match")))))
